6a6824d48aec3b47fcb9510a662c966975e49230
Quyto89 <Quyto89@users.noreply.github.com>
2018-09-18 14:10:09 +0700

    quy commit

--- src/BTNhom/ChuNhat.java
+++ src/BTNhom/ChuNhat.java
@@ -1,9 +1,27 @@
 package BTNhom;
 
 public class ChuNhat {
+	
+	private int canh1;
+	private int canh2;
+	
 
-	public ChuNhat() {
+	public ChuNhat(int canh1, int canh2) {
 		// TODO Auto-generated constructor stub
+		this.canh1 = canh1;
+		this.canh2 = canh2;
+	}
+	
+	public ChuNhat(int canh1) {
+		// TODO Auto-generated constructor stub
+		this.canh1 = canh1;
 	}
 
+	public int tinhChuvi (int canh1, int canh2) {
+		return (canh1 + canh2)*2;
+	}
+	
+	public int tinhDientich (int canh1, int canh2) {
+		return (canh1 * canh2);
+	}
 }
