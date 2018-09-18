2add097aa18f729d23f6c69858b6e945eee41416
luu.thi.hoai.thuong <luu.thi.hoai.thuong@framgia.com>
2018-09-18 13:09:04 +0700

    Hoai Thuong add class Hinh Tron

--- src/BTNhom/HinhTron.java
+++ src/BTNhom/HinhTron.java
@@ -2,8 +2,19 @@ package BTNhom;
 
 public class HinhTron {
 
-	public HinhTron() {
-		// TODO Auto-generated constructor stub
-	}
+	private double banKinh;
 
+	public HinhTron(float banKinh) {
+		this.banKinh = banKinh;
+	}
+	
+	public double Tinh_Dien_Tich()
+	{
+		return (banKinh*banKinh)*3.14;
+	}
+	
+	public double Tinh_Chu_Vi()
+	{
+		return banKinh*2*3.14;
+	}
 }
